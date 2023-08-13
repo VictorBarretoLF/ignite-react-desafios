04d9aab0c0b89a10848fe67df0e731c457bd1b6b
Víctor Barreto <victorbarretolins@gmail.com>
2023-08-13 16:09:29 -0300

    feat: criado o componente de header

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -2,13 +2,15 @@ import todoLogo from "./assets/todo-logo.svg";
 import styles from "./styles.module.css";
 import { AddNewTodo } from "./components/AddNewTodo";
 import { TodosInfo } from "./components/TodosInfo";
+import { Icons } from "./components/Icons/icons";
+import { Header } from "./components/Header";
+import { useState } from "react";
 
 function App() {
+	const [todos, setTodos] = useState([]);
 	return (
 		<div>
-			<header className={styles.header}>
-				<img src={todoLogo} alt="todo logo" />
-			</header>
+			<Header />
 
 			<main
 				style={{
@@ -20,6 +22,27 @@ function App() {
 				<AddNewTodo />
 				<section>
 					<TodosInfo />
+
+					<div className={styles.todoList}>
+						{todos.length === 0 ? (
+							"nada"
+						) : (
+							<div className={styles.todo}>
+								<button type="button" className={styles.todoCheckButton}>
+									<div></div>
+								</button>
+
+								<p>
+									Integer urna interdum massa libero auctor neque turpis turpis semper.
+									Duis vel sed fames integer.
+								</p>
+
+								<button type="button" className={styles.deleteButton}>
+									<Icons.trash />
+								</button>
+							</div>
+						)}
+					</div>
 				</section>
 			</main>
 		</div>
--- todo-list/src/components/Icons/icons.tsx
+++ todo-list/src/components/Icons/icons.tsx
@@ -1,5 +1,7 @@
-import { PlusCircle } from "lucide-react";
+import { PlusCircle, CheckCircle2, Trash2 } from "lucide-react";
 
 export const Icons = {
 	plus: PlusCircle,
+	checkCircle: CheckCircle2,
+	trash: Trash2,
 };
