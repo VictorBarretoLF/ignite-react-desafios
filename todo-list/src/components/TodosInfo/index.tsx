7bb141be8f70075e2c4c0ada21c28f0c604ece67
Víctor Barreto <victorbarretolins@gmail.com>
2023-08-13 19:23:24 -0300

    feat: traking the completed todos and alltodos in the TodosInfo component

--- todo-list/src/components/TodosInfo/index.tsx
+++ todo-list/src/components/TodosInfo/index.tsx
@@ -6,16 +6,27 @@ type Props = {
 };
 
 export function TodosInfo({ todos }: Props) {
+	const allTodos = todos.length;
+	const completedTodos = todos.filter((todo) => todo.isCompleted).length;
+
 	return (
 		<header className={styles.TodoInfoHeader}>
 			<div className={styles.todoInfo}>
 				<p>Tarefas criadas</p>
-				<span>{todos.length}</span>
+				<span>{allTodos}</span>
 			</div>
 
 			<div className={styles.todoInfo}>
 				<p className={styles.textPurple}>Concluídas</p>
-				<span>0</span>
+				<span>
+					{allTodos === 0 && completedTodos == 0 ? (
+						0
+					) : (
+						<>
+							{completedTodos} de {allTodos}
+						</>
+					)}
+				</span>
 			</div>
 		</header>
 	);
