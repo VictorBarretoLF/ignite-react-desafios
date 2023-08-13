670322c1637a6fe98954ba98e70f43e6f4a43bd8
Víctor Barreto <victorbarretolins@gmail.com>
2023-08-13 16:54:55 -0300

    feat: setting the title when change the input

--- todo-list/src/components/AddNewTodo/index.tsx
+++ todo-list/src/components/AddNewTodo/index.tsx
@@ -1,13 +1,28 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Icons } from "../Icons/icons";
 import styles from "./newtodo.module.css";
 
 export function AddNewTodo() {
+	const [title, setTitle] = useState("");
+
+	function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+
+		setTitle("");
+	}
+
+	function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+		setTitle(event.target.value);
+	}
+
 	return (
-		<form action="" className={styles.addNewTodoForm}>
+		<form onSubmit={handleSubmit} className={styles.addNewTodoForm}>
 			<input
 				placeholder="Adicione uma nova tarefa"
 				type="text"
+				value={title}
 				className={styles.addNewTodoInput}
+				onChange={onChangeTitle}
 			/>
 			<button className={styles.addNewTodoButton}>
 				Criar <Icons.plus />
