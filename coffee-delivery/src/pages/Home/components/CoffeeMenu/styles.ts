494c1ddd2b2f75e3607b72b32a52563b82b436c2
˜Victor <victorbarretolins@gmail.com>
2023-08-26 16:41:31 -0300

    chore(CoffeeMenu/styles.ts): making the coffee list responsive using grid

--- coffee-delivery/src/pages/Home/components/CoffeeMenu/styles.ts
+++ coffee-delivery/src/pages/Home/components/CoffeeMenu/styles.ts
@@ -7,7 +7,11 @@ export const CoffeeMenuContainer = styled.section`
 
 export const CoffeeList = styled.div`
     width: 100%;
+
     display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+    column-gap: 2rem;
+    row-gap: 2.5rem;
 
     margin-top: 3.5rem;
 `;
